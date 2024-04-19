5e96841ef536f0723943a50e53ca2264dde8b376
Saifreid <129993771+Saifreid@users.noreply.github.com>
2024-04-19 19:50:02 -0400

    Fully functional login page

--- backEnd/app.js
+++ backEnd/app.js
@@ -50,6 +50,7 @@ app.get('/getUsers', (req, res) => {
       run().catch(console.dir);
 })
 
+//Basic add user the one used for project is /registerUser
 
 app.post('/addUser', async function(req, res) {
     const {MongoClient, ServerApiVersion} = require('mongodb');
@@ -78,6 +79,96 @@ app.post('/addUser', async function(req, res) {
     }
     run().catch(console.dir);
 })
+
+app.post('/checkLogin', async (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', "*");
+
+    const bcrypt = require('bcrypt');
+    const uri = process.env.uri;
+    const { MongoClient, ServerApiVersion } = require('mongodb');
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+    const user = {username: req.body.username};
+
+    try {
+        await client.connect();
+        await client.db("ETI461").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        const result = await client.db("ETI461").collection("Users").findOne(user);
+        
+        if (result) {
+            const passwordMatch = await bcrypt.compare(req.body.password, result.password);
+            if (passwordMatch) {
+                res.cookie("user", result.username, { maxAge: 1000 * 60 * 60 * 24 });
+                res.cookie("pass", result.password, { maxAge: 1000 * 60 * 60 * 24 });
+                res.json([true, "user=" + result.username + "&pass=" + result.password]);
+            } else {
+                res.json([false]);
+            }
+        } else {
+            res.json([false]);
+        }
+    } catch (err) {
+        console.error(err);
+    } finally {
+        await client.close();
+    }
+});
+
+
+
+app.post('/registerUser', async function(req, res) {
+    const {MongoClient, ServerApiVersion} = require('mongodb');
+    const uri = process.env.uri;
+    const bcrypt = require('bcrypt');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+    
+    async function run(){
+        
+        try{
+            var user = req.body.username;
+            var pass = req.body.password;
+            
+            console.log(req.body);
+            console.log("Before run()");
+            await client.connect();
+            await client.db("ETI461").command({ping: 1});
+            console.log("Pinged your deployment. You successfully connected to MongoDB!");
+            const existingUser = await client.db("ETI461").collection("Users").findOne({ username: user });
+            if (existingUser){
+                res.json({success: false, message: "Username already exists"});
+            } else{
+                const hashedPassword = await bcrypt.hash(pass, 10);
+                const item = { username: user, password: hashedPassword, cart: [] };
+                const insertionResult = await client.db("ETI461").collection("Users").insertOne(item);
+                res.json({ success: true, message: "User registered successfully" });
+            }
+        } catch(err){
+            console.error(err);
+            res.status(500).json({ success: false, message: "An error occurred while registering the user" });
+        }  
+        finally{
+            await client.close();
+        }
+    }
+    run().catch(console.dir);
+})
+
+
 
 
 
--- backEnd/signIn.js
+++ backEnd/signIn.js
@@ -2,11 +2,30 @@
 
 
 function signIn(){
-    $(document).ready(function(){
-        $.get("http://localhost:3000" + '/getUsers', function(data){
-            console.log(data);
-        });
+    const user = $("#username").val();
+    const pass = $("#password").val();
+    console.log(user, " ", pass);
+    if (user == ""){
+        alert("Please enter a valid username!");
+        return false;
+    };
+    if (pass == ""){
+        alert("Please enter a valid password!");
+        return false;
+    };
+    $.post("http://localhost:3000/checkLogin", {'username': user, 'password': pass}, function(data){
+        console.log(data);
+
+        if (data[0]){
+            console.log("User exists");
+            window.location = "index.html";
+        } else{
+            console.log("User does not exist");
+            alert("Username and password combination does not exist");
+            $("#messageArea").html("Username and password combination does not exist");
+        };
     });
+
 }
 
 function signUp(){
@@ -17,4 +36,22 @@ function signUp(){
     $.post("http://localhost:3000/addUser", userJSON, function(data){
         console.log(data);
     }) 
+}
+
+function register(){
+    var user = $("#username").val();
+    var pass = $("#password").val();
+    var userJSON = {'username': user, 'password': pass};
+    console.log(user, pass);
+    $.post("http://localhost:3000/registerUser", userJSON, function(data){
+        console.log(data);
+    }) 
+}
+
+function showUsers(){
+    $(document).ready(function(){
+        $.get("http://localhost:3000" + '/getUsers', function(data){
+            console.log(data);
+        });
+    });
 }
