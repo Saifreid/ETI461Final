e5e1e4d8584eb8da1cf598de35f1268518aec033
Saifreid <129993771+Saifreid@users.noreply.github.com>
2024-04-19 00:36:33 -0400

    signIn page almost completely funcitonal it achieves reading from database, aswell as creating new users to database.

--- backEnd/app.js
+++ backEnd/app.js
@@ -5,23 +5,26 @@ var cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
 const dotenv = require("dotenv").config();
-
+var bodyParser = require('body-parser');
 
 
 var app = express();
 
 const port = 3000;
-
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 
 const uri = process.env.uri;
 
 
 app.get('/getUsers', (req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-type', null);
-
+    res.setHeader('Content-type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
+    const { MongoClient, ServerApiVersion } = require('mongodb');
     const client = new MongoClient(uri, {
         serverApi: {
             version: ServerApiVersion.v1,
@@ -45,6 +48,35 @@ app.get('/getUsers', (req, res) => {
         }
       }
       run().catch(console.dir);
+})
+
+
+app.post('/addUser', async function(req, res) {
+    const {MongoClient, ServerApiVersion} = require('mongodb');
+    const uri = process.env.uri;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+    const item = {username: req.body.username, password: req.body.password, cart: []};
+    console.log(req.body);
+    console.log(item);
+    async function run(){
+        try{
+            await client.connect();
+            await client.db("ETI461").command({ping: 1});
+            console.log("Pinged your deployment. You successfully connected to MongoDB!");
+            const result = await client.db("ETI461").collection("Users").insertOne(item);
+            res.json(result);
+        }   finally{
+            await client.close();
+        }
+    }
+    run().catch(console.dir);
 })
 
 
--- backEnd/signIn.js
+++ backEnd/signIn.js
@@ -3,8 +3,18 @@
 
 function signIn(){
     $(document).ready(function(){
-        $.get("http://localhost:3000"+ '/getUsers', function(data){
+        $.get("http://localhost:3000" + '/getUsers', function(data){
             console.log(data);
         });
     });
+}
+
+function signUp(){
+    var user = $("#username").val();
+    var pass = $("#password").val();
+    var userJSON = {'username': user, 'password': pass};
+    console.log(user, pass);
+    $.post("http://localhost:3000/addUser", userJSON, function(data){
+        console.log(data);
+    }) 
 }
